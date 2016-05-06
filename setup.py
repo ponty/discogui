a8a84009978983db6e3a38da7c2ef19d808927c7
ponty <ponty@home>
2016-05-06 14:20:49 +0200

    fix py versions in setup.py

--- setup.py
+++ setup.py
@@ -39,13 +39,13 @@ classifiers = [
     'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     #    "Programming Language :: Python :: 2 :: Only",
-        "Programming Language :: Python :: 3",
+#        "Programming Language :: Python :: 3",
 #     'Programming Language :: Python :: 3.0',
     #     "Programming Language :: Python :: 3.1",
 #     'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+#    'Programming Language :: Python :: 3.3',
+#    'Programming Language :: Python :: 3.4',
+#    'Programming Language :: Python :: 3.5',
 ]
 
 install_requires = open("requirements.txt").read().split('\n')
