ed8b8c7508a80392f6bbfe5a75a33a067645b68f
ponty <ponty@home>
2020-05-21 20:48:44 +0200

    setup: remove use_2to3

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ __version__ = None
 exec(open(os.path.join(NAME, "about.py")).read())
 VERSION = __version__
 
-extra = {}
-if sys.version_info >= (3,):
-    extra["use_2to3"] = True
-    extra["use_2to3_exclude_fixers"] = ["lib2to3.fixes.fix_import"]
+# extra = {}
+# if sys.version_info >= (3,):
+#     extra["use_2to3"] = True
+#     extra["use_2to3_exclude_fixers"] = ["lib2to3.fixes.fix_import"]
 
 classifiers = [
     # Get more strings from
@@ -50,5 +50,5 @@ setup(
     license="BSD",
     packages=PACKAGES,
     install_requires=install_requires,
-    **extra
+    # **extra
 )
