6379a0b8b39e185ed8cd3d787b193f87d34f86e3
ponty <ponty@home>
2020-05-27 17:59:11 +0200

    test_examples fix

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,9 +1,18 @@
-from discogui.examples import buttondiscovery, clickbutton, hovergnumeric
+import sys
+
+from backports.tempfile import TemporaryDirectory
+from easyprocess import EasyProcess
+
+py = sys.executable
+
+
+def run_mod(mod, cwd):
+    p = EasyProcess([py, "-m", mod], cwd=cwd).call()
+    assert p.return_code == 0
 
 
 def test_examples():
-    clickbutton.main()
-
-    #    gui examples, not run
-    hovergnumeric.main
-    buttondiscovery.main
+    with TemporaryDirectory() as tmpdirname:
+        run_mod("discogui.examples.buttondiscovery", tmpdirname)
+        run_mod("discogui.examples.hovergnumeric", tmpdirname)
+        run_mod("discogui.examples.clickbutton", tmpdirname)
